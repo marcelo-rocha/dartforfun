3b476316775fdd8e5b8e327bf9eda26f4c32a558
Marcelo Rocha <marcelo.rocha@bematech.com>
2016-04-19 21:38:51 -0300

    otimization

--- atbash/atbash.dart
+++ atbash/atbash.dart
@@ -3,8 +3,9 @@ import "dart:io";
 main() {
   var line;
   while ((line = stdin.readLineSync()) != null) {
-    print(line.splitMapJoin(new RegExp("[A-Za-z]"),
-        onMatch: (m) => new String.fromCharCode(
+    print(line.replaceAllMapped(
+        new RegExp("[A-Za-z]"),
+        (Match m) => new String.fromCharCode(
             (m[0].codeUnitAt(0) <= 90 ? 155 : 219) - m[0].codeUnitAt(0))));
   }
 }

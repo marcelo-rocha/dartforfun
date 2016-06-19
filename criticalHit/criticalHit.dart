5ec8574bdf57db67fd0f1216f28a66af1a4ce81d
Marcelo Rocha <marcelo.rocha@bematech.com>
2016-06-19 13:35:16 -0300

    challenge 271 [easy]

---
+++ criticalHit/criticalHit.dart
@@ -0,0 +1,22 @@
+import "dart:math";
+
+var input = [
+  [4, 1],
+  [4, 4],
+  [4, 5],
+  [4, 6],
+  [1, 10],
+  [100, 200],
+  [8, 20]
+];
+
+double P(d, h) => h <= 1 ? 1.0 : 1 / d * P(d, h - d) + max(d - h, 0) * 1 / d;
+String pad(v, width) => v.toString().padLeft(width);
+
+void main() {
+  print("  d |   h |     p");
+  for (var i = 0; i < input.length; i++) {
+    print(
+        "${pad(input[i][0], 3)} | ${pad(input[i][1], 3)} | ${pad(P(input[i][0], input[i][1]), 11)}");
+  }
+}
---
+++ criticalHit/readme.md
@@ -0,0 +1,1 @@
+resolution of daily programmer challenge 271 (Easy) Critical Hit https://www.reddit.com/r/dailyprogrammer/comments/4nvrnx/20160613_challenge_271_easy_critical_hit/
